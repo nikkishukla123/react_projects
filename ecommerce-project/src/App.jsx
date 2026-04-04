384862b6c3e53eb315b96bdbd29b9e95a96abaa0
nikkishukla123 <ritikshukla1987@gmail.com>
2026-04-04 14:40:09 +0530

    `Added new routes andrefactored header component `

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
-import { Routes, Route } from 'react-router'
-import { HomePage } from './Pages/HomePage'
-import { CheckoutPage } from './Pages/CheckoutPage'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './Pages/HomePage';
+import { CheckoutPage } from './Pages/CheckoutPage';
+import { OrdersPage } from './Pages/OrdersPage';
+import { TrackingPage } from './Pages/TrackingPage';
 import './App.css'
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Routes>
     <Route index element={<HomePage/>}/>
     <Route path="checkout" element={<CheckoutPage/>}/>
+    <Route path="orders" element={<OrdersPage/>}/>
+    <Route path="tracking" element={<TrackingPage/>}/>
     </Routes>
     </>
   )
